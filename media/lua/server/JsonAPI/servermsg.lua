59ab4c3f5ca3cb78ffc937c5e9cb2a07218bf243
Josh Greuling <nooska@robojanus.com>
2026-05-16 17:19:58 -0500

    Fix servermsg: use GameServer.sendAdminMessage

--- media/lua/server/JsonAPI/servermsg.lua
+++ media/lua/server/JsonAPI/servermsg.lua
@@ -3,7 +3,7 @@ if isClient() then return end
 local function handleServerMsg(args)
     local message = args.message
     if not message then return '{"error":"missing arg: message"}' end
-    ChatServer.getInstance():sendServerAlertMessageToServerChat(message)
+    GameServer.sendAdminMessage(message, -1, -1, -1)
     return '{"sent":true,"message":"' .. JsonAPI.jsonEscape(message) .. '"}'
 end
 
