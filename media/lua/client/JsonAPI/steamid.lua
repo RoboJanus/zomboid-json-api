88f89cd8f2ba0ee0b71a65742080f9655258c076
Josh Greuling <nooska@robojanus.com>
2026-05-23 11:43:24 -0500

    Debug: add prints to client steamid script

--- media/lua/client/JsonAPI/steamid.lua
+++ media/lua/client/JsonAPI/steamid.lua
@@ -1,12 +1,25 @@
 if isServer() then return end
 
+print("[JsonAPI] Client steamid.lua loaded")
+
 local function onConnected()
+    print("[JsonAPI] OnConnected fired")
     local player = getPlayer()
-    if not player then return end
-    local steamId = getSteamIDFromUsername(player:getUsername())
+    if not player then
+        print("[JsonAPI] ERROR: getPlayer() returned nil")
+        return
+    end
+    local username = player:getUsername()
+    print("[JsonAPI] Username: " .. tostring(username))
+    local steamId = getSteamIDFromUsername(username)
+    print("[JsonAPI] getSteamIDFromUsername result: " .. tostring(steamId))
     if steamId then
         sendClientCommand("JsonAPI", "steamId", {steamId = steamId})
+        print("[JsonAPI] Sent steamId to server: " .. steamId)
+    else
+        print("[JsonAPI] ERROR: getSteamIDFromUsername returned nil")
     end
 end
 
 Events.OnConnected.Add(onConnected)
+print("[JsonAPI] OnConnected handler registered")
