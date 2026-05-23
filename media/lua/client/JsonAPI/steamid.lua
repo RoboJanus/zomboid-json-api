16afbb868f1dbb71f3c38b4128053113ea997674
Josh Greuling <nooska@robojanus.com>
2026-05-23 13:30:55 -0500

    Debug: try multiple Steam ID approaches on client

--- media/lua/client/JsonAPI/steamid.lua
+++ media/lua/client/JsonAPI/steamid.lua
@@ -1,25 +1,39 @@
 if isServer() then return end
 
-print("[JsonAPI] Client steamid.lua loaded")
-
 local function onGameStart()
-    print("[JsonAPI] OnGameStart fired")
     local player = getPlayer()
-    if not player then
-        print("[JsonAPI] ERROR: getPlayer() returned nil in OnGameStart")
-        return
-    end
+    if not player then return end
     local username = player:getUsername()
-    print("[JsonAPI] Username: " .. tostring(username))
-    local steamId = getSteamIDFromUsername(username)
-    print("[JsonAPI] getSteamIDFromUsername result: " .. tostring(steamId))
-    if steamId then
-        sendClientCommand("JsonAPI", "steamId", {steamId = steamId})
-        print("[JsonAPI] Sent steamId to server: " .. steamId)
-    else
-        print("[JsonAPI] ERROR: getSteamIDFromUsername returned nil")
+
+    -- Try various approaches to get Steam ID
+    local sid1 = tostring(player:getSteamID())
+    print("[JsonAPI] tostring(getSteamID): " .. sid1)
+
+    local sid2 = string.format("%.0f", player:getSteamID())
+    print("[JsonAPI] format %.0f: " .. sid2)
+
+    local sid3 = string.format("%d", player:getSteamID())
+    print("[JsonAPI] format %d: " .. sid3)
+
+    -- Try getSteamIDFromUsername with different inputs
+    local sid4 = getSteamIDFromUsername(username)
+    print("[JsonAPI] getSteamIDFromUsername(username): " .. tostring(sid4))
+
+    -- Try with Steam profile name
+    local ok, steamName = pcall(function() return getSteamProfileName and getSteamProfileName() end)
+    print("[JsonAPI] getSteamProfileName: " .. tostring(steamName))
+
+    if ok and steamName then
+        local sid5 = getSteamIDFromUsername(steamName)
+        print("[JsonAPI] getSteamIDFromUsername(steamName): " .. tostring(sid5))
+    end
+
+    -- Send whatever we got
+    local bestId = sid4 or sid3
+    if bestId and bestId ~= "nil" then
+        sendClientCommand("JsonAPI", "steamId", {steamId = bestId})
+        print("[JsonAPI] Sent: " .. bestId)
     end
 end
 
 Events.OnGameStart.Add(onGameStart)
-print("[JsonAPI] OnGameStart handler registered")
