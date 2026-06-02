c2f57559558d8dbb158a9329b97fdfab8cd4e734
Josh Greuling <nooska@robojanus.com>
2026-06-02 00:56:18 -0500

    Show delivery notification in chat panel (same as servermsg) instead of speech bubble

--- media/lua/client/JsonAPI/additem_client.lua
+++ media/lua/client/JsonAPI/additem_client.lua
@@ -1,10 +1,18 @@
 local function onServerCommand(module, command, args)
     if module ~= "JsonAPI" then return end
     if command == "itemDelivered" and args and args.message then
-        local player = getPlayer()
-        if player then
-            player:Say(args.message)
-        end
+        if not ISChat.instance or not ISChat.instance.chatText then return end
+        local message = "<RGB:0.6,0.9,1> [Server] " .. args.message
+        local msg = {
+            getText = function(_) return message end,
+            getTextWithPrefix = function(_) return message end,
+            isServerAlert = function(_) return true end,
+            isShowAuthor = function(_) return false end,
+            getAuthor = function(_) return "" end,
+            setShouldAttractZombies = function(_) return false end,
+            setOverHeadSpeech = function(_) return false end,
+        }
+        ISChat.addLineInChat(msg, 0)
     end
 end
 
